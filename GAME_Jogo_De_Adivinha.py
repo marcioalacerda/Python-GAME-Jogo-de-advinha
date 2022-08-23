8c28db2d8ec33b049a5e6f69170eae2397812369
Marcio Lacerda <marcioalacerda2201@gmail.com>
2022-08-23 08:56:12 -0300

    Melhorando o jogo, onde o computador vai pensar em um número entre 0 a 10 que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários.

--- GAME_Jogo_De_Adivinha.py
+++ GAME_Jogo_De_Adivinha.py
@@ -1,19 +1,27 @@
-'''Programa que faz o computador "pensar" em um número inteiro entre 0 e 5  e pede para o usuário
-tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.'''
-
-from random import randrange
+'''Melhorando o jogo, onde o computador vai "pensar" em um número entre 0 a 10 que agora o jogador vai
+tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários.'''
+from random import randint
 from time import sleep # faz o computador esperar em segundos
 from termcolor import colored # para dar cor no seu programa
 
-computador = randrange(0, 6) # faz o computador "PENSAR"
 print(colored('-=-', 'yellow') * 20)
-print('  Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('  Ola! Sou seu computador, Vamos jogar?')
+print('  Vou pensar em um número entre 0 e 10. Tente adivinhar...')
 print(colored('-=-', 'yellow') * 20)
-jogador = int(input('Em que número eu pensei? ')) # jogador tenta adivinhar
-print(colored('PROCESSANDO...', 'magenta'))
-sleep(3)
-if jogador == computador:
-    print(colored('PARABÉNS! Você conseguiu me vencer!', 'green'))
-else:
-    print(colored('GANHEI! Eu pensei no número {} e não no {}!', 'red').format(computador, jogador))
+computador = randint(0, 10)  # faz o computador "PENSAR"
+acertou = False
+palpite = 0
 
+while not acertou:
+    palpite += 1
+    jogador = int(input('Em que número eu pensei? ')) # jogador tenta adivinhar
+    print(colored('PROCESSANDO...', 'magenta'))
+    sleep(1)
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez! ')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez! ')
+print(colored('PARABÉNS! Você conseguiu acertar com {} tentativas!', 'green').format(palpite))
